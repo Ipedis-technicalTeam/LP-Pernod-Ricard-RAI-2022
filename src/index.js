7717edf0e9a0fcfbad73d0acd1ffc61d12cb11a8
prakash <prakash@ipedis.com>
2022-11-14 08:51:44 +0400

    lp-pernod - recover changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import "./assets/sass/index.scss";
 import "./js/skipLinks";
 import "./js/translation";
-import "./js/cookies";
 import "../node_modules/ip-d14-slider/dist/ip-slider/ip-slider.esm";
 
 // Only For Lang Switch Btn Demo
 // import "./js/switchLang";
+
+import simpleParallax from 'simple-parallax-js';
+var image = document.getElementsByClassName('thumbnail');
+var imageUp = document.getElementsByClassName('thumbnail-up');
+
+new simpleParallax(image, {
+    orientation: 'down',
+    overflow: true,
+    transition: 'cubic-bezier(0,0,0,1)'
+});
+
+new simpleParallax(imageUp, {
+    orientation: 'up',
+    overflow: true,
+    transition: 'cubic-bezier(0,0,0,1)'
+});
